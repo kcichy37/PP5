344ad2f3a63675161d2925173b68411fe1521f43
kcichy37 <kcichy37@gmail.com>
2023-02-15 22:00:37 +0000

    Add rest of food categories and add them to cards

--- food_products/views.py
+++ food_products/views.py
@@ -8,9 +8,25 @@ def food_menu(request):
     A view to show the food products
     """
     starters = FoodProduct.objects.filter(category='1')
+    salads = FoodProduct.objects.filter(category='2')
+    pizza = FoodProduct.objects.filter(category='3')
+    pasta = FoodProduct.objects.filter(category='4')
+    risotto = FoodProduct.objects.filter(category='5')
+    meat_fish = FoodProduct.objects.filter(category='7')
+    sides = FoodProduct.objects.filter(category='8')
+    desserts = FoodProduct.objects.filter(category='9')
+    soft_drinks = FoodProduct.objects.filter(category='10')
 
     context = {
         'starters': starters,
+        'salads': salads,
+        'pizza': pizza,
+        'pasta': pasta,
+        'risotto': risotto,
+        'meat_fish': meat_fish,
+        'sides': sides,
+        'desserts': desserts,
+        'soft_drinks': soft_drinks,
     }
 
     return render(request, 'menu/menu.html', context)
