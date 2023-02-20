529a178139ad9d16e0fe158224a16abe92ac8f26
kcichy37 <kcichy37@gmail.com>
2023-02-20 16:09:05 +0000

    Add function so page doesnt refresh after adding products

--- bag/context.py
+++ bag/context.py
@@ -5,12 +5,10 @@ from food_products.models import FoodProduct
 
 
 def bag_contents(request):
-
     bag_items = []
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
-
     for item_id, quantity in bag.items():
         product = get_object_or_404(FoodProduct, pk=item_id)
         total += quantity * product.price
@@ -18,18 +16,15 @@ def bag_contents(request):
         bag_items.append({
             'item_id': item_id,
             'quantity': quantity,
-            'product': product
+            'product': product,
         })
-
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-
     grand_total = delivery + total
-
     context = {
         'bag_items': bag_items,
         'total': total,
@@ -39,5 +34,4 @@ def bag_contents(request):
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
-
     return context
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -13,15 +14,21 @@ def add_to_bag(request, item_id):
     """
     Add product to the bag
     """
-    quantity = int(request.POST.get('quantity'))
-    bag = request.session.get('bag', {})
-    redirect_url = request.POST.get('redirect_url')
+    if request.method == 'POST':
+        quantity = int(request.POST.get('quantity'))
+        bag = request.session.get('bag', {})
+        redirect_url = request.POST.get('redirect_url')
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
-    request.session['bag'] = bag
-    
-    return redirect(redirect_url)
+        request.session['bag'] = bag
+
+        # Create a JSON response that includes the updated bag data
+        response_data = {
+            'success': True,
+            'bag': bag
+        }
+        return JsonResponse(response_data)
