f96857357c494323feae2fec48d0fa2e135d8e44
kcichy37 <kcichy37@gmail.com>
2023-02-22 14:33:11 +0000

    Fix functionality for remove and update buttons

--- bag/views.py
+++ bag/views.py
@@ -27,29 +27,35 @@ def add_to_bag(request, item_id):
 
 
 def adjust_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
+    """Adjust the quantity of the specified product to the specified amount"""
 
+    print('Item ID:', item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
-    if quantity > 0:
-        bag[item_id] = quantity
+    if item_id in list(bag.keys()):
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
     else:
-        bag.pop(item_id)
+        bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
 
 def remove_from_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
-
-    quantity = int(request.POST.get('quantity'))
-    bag = request.session.get('bag', {})
+    """Remove the item from the shopping bag"""
 
     try:
-        bag.pop(item_id)
-        request.session['bag'] = bag
-        return HttpResponse(status=200)
+        bag = request.session.get('bag', {})
+        if item_id in bag:
+            del bag[item_id]
+            request.session['bag'] = bag
+            return HttpResponse(status=200)
+        else:
+            return HttpResponse(status=404)
+
     except Exception as e:
         return HttpResponse(status=500)
