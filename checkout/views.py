bf64911d4d183ef2ebf0763f028f6bd877879899
kcichy37 <kcichy37@gmail.com>
2023-02-23 20:37:04 +0000

    Correct checkout success view

--- checkout/views.py
+++ checkout/views.py
@@ -129,23 +129,24 @@ def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
-    profile = UserProfile.objects.get(user=request.user)
-    # Attach the user's profile to the order
-    order.user_profile = profile
-    order.save()
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
 
-    if save_info:
-        profile_data = {
-            'default_full_name': order.full_name,
-            'default_email': order.email,
-            'default_phone_number': order.phone_number,
-            'default_postcode': order.postcode,
-            'default_street_address1': order.street_address1,
-            'default_street_address2': order.street_address2,
-        }
-        user_profile_form = UserProfileForm(profile_data, instance=profile)
-        if user_profile_form.is_valid():
-            user_profile_form.save()
+        if save_info:
+            profile_data = {
+                'default_full_name': order.full_name,
+                'default_email': order.email,
+                'default_phone_number': order.phone_number,
+                'default_postcode': order.postcode,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
 
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
