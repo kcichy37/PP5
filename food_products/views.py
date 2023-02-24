d1beab14fbf34b27ae9955de888d2c43a2f3ac93
kcichy37 <kcichy37@gmail.com>
2023-02-24 19:23:32 +0000

    Complete admin add product functionality

--- food_products/views.py
+++ food_products/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import FoodProduct, FoodCategory
 from .forms import ProductForm
+from django.contrib import messages
 # Create your views here.
 
 
@@ -21,7 +22,17 @@ def add_product(request):
     """
     Add a product to the store
     """
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid')
+    else:
+        form = ProductForm()
+
     template = 'menu/add_product.html'
     context = {
         'form': form,
