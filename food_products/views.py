8db6eb534bd6f0f0bd2680281652b672b327cf0a
kcichy37 <kcichy37@gmail.com>
2023-02-24 21:07:10 +0000

    Add product edit and delete buttons for admins

--- food_products/views.py
+++ food_products/views.py
@@ -40,7 +40,7 @@ def add_product(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('menu'))
+            return redirect(reverse('all_products'))
         else:
             messages.error(
                 request, 'Failed to add product. Please ensure the form is valid')
