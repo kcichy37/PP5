c434306d464f338db445a0cb20861dd7a3ca9c2f
kcichy37 <kcichy37@gmail.com>
2023-02-25 01:34:35 +0000

    Add image friendly widget

--- food_products/forms.py
+++ food_products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import FoodProduct, FoodCategory
 
 
@@ -8,11 +9,11 @@ class ProductForm(forms.ModelForm):
         model = FoodProduct
         fields = '__all__'
 
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = FoodCategory.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'stripe-style-input'
