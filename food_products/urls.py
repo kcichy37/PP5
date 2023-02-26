3dced0695b211de08314b6643293dd61f6c9952c
kcichy37 <kcichy37@gmail.com>
2023-02-26 15:30:26 +0000

    Fix issue with quantity not passing as int()

--- food_products/urls.py
+++ food_products/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.food_menu, name='menu'),
-    path('add/', views.add_product, name='add_product'),
+    path('add/<int:product_id>', views.add_product, name='add_product'),
     path('edit/<int:product_id>', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>', views.delete_product, name='delete_product'),
     path('all_products/', views.all_products, name='all_products'),
