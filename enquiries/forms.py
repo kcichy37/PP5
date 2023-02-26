8ac500e611b5a9679f8e1e96194c8532aac0d1c5
kcichy37 <kcichy37@gmail.com>
2023-02-26 17:55:02 +0000

    Style all_product template and add responsivness

--- enquiries/forms.py
+++ enquiries/forms.py
@@ -23,10 +23,11 @@ class EnquiryForm(forms.ModelForm):
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-        self.fields[field].widget.attrs['placeholder'] = placeholder
-        self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-        self.fields[field].label = False
+            if field != '':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
+            self.fields['image'].label = 'Image - if enquiry relates to the order.'
