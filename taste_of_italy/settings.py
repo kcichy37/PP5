55a23e2c83b1d7910f4fcc44db8ea21264c0fc5f
kcichy37 <kcichy37@gmail.com>
2023-02-27 08:58:40 +0000

    Add description and keywords meta tags

--- taste_of_italy/settings.py
+++ taste_of_italy/settings.py
@@ -212,7 +212,7 @@ STRIPE_WH_SECRET = os.environ.get("STRIPE_WH_SECRET")
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-    DEFAULT_FROM_EMAIL = "TasteofItaly@email.com"
+    DEFAULT_FROM_EMAIL = "tasteofitalyproject@gmail.com"
 else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_USE_TLS = True
